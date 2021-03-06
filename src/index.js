0b6c830c8d5c48fad5fecda3273c3190dcba716c
Marco Chavez <mxrcochxvez@protonmail.com>
2021-03-06 00:27:28 -0800

    setting up navigation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
 import { render } from "react-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import routes from './routes';
 import './style.css';
 
 function App() {
-    const [state, setState] = useState("CLICK ME");
-
-    return <button onClick={() => setState("CLICKED")}>{state}</button>;
+    return (
+        <Router>
+            <h1>Navbar would go here.</h1>
+            <Switch>
+                {routes.map(({ path, component }) => (
+                    <Route path={path} component={component} />
+                ))}
+            </Switch>
+        </Router>
+    )
 }
 
 render(<App />, document.getElementById("root"));
